fe613836d330d32dc798e7543deb418e0b18e3d6
nayonsoso <panda0329@naver.com>
2023-07-31 15:45:20 +0900

    @RestControllerAdvice, @ExceptionHandler 적용

---
+++ src/main/java/com/example/weather/error/InvalidDate.java
@@ -0,0 +1,9 @@
+package com.example.weather.error;
+
+public class InvalidDate extends RuntimeException {
+    private static final String MESSAGE = "너무 미래의 날짜입니다.";
+
+    public InvalidDate(){
+        super(MESSAGE);
+    }
+}
